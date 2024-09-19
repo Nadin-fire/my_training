992ab033262230c0d25ff506eb7f203c44b4f657
Nadin-fire <ioyanvm@gmail.com>
2024-09-20 00:58:18 +0800

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+quantity_of_tasks = 12
+quantity_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = quantity_of_hours/quantity_of_tasks
+print('Курс:', course_name + ',', 'всего задач:', str(quantity_of_tasks) + ',', 'затрачено часов:', str(quantity_of_hours) + ',', 'среднее время выполнения:', time_for_one_task, 'часа' + '.')
